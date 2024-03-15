8830044cd0c00ad58f037b14ccec7ed9b2b0399b
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-03-16 03:28:56 +0500

    beautifulsoup

--- data collection.py
+++ data collection.py
@@ -1,5 +1,7 @@
 import pandas as pd 
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-poke = pd.read_csv('pokemon_data.csv')
-
-print(poke.head(1))
+date = "Please enter a date in the following format MM/DD/YYYY : "
+page = requests.get("https://www.yallakora.com/Match-Center/?date=12/13/2023")
