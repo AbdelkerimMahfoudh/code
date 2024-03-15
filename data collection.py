8e85145c7fcd78fd9f70533f8220954874dc133f
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-03-16 03:37:34 +0500

    pandas rmv

--- data collection.py
+++ data collection.py
@@ -1,7 +1,13 @@
-import pandas as pd 
+# import pandas as pd 
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 date = "Please enter a date in the following format MM/DD/YYYY : "
 page = requests.get(f"https://www.yallakora.com/Match-Center/?date={date}")
+
+def main (page) :
+    src = page.content
+    print(src)
+
+ 
