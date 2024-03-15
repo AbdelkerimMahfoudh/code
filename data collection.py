8f8910b67682ab0cc1ed8d02e587d67d89dd461b
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-03-16 03:29:45 +0500

    date

--- data collection.py
+++ data collection.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 import csv
 
 date = "Please enter a date in the following format MM/DD/YYYY : "
-page = requests.get("https://www.yallakora.com/Match-Center/?date=12/13/2023")
+page = requests.get(f"https://www.yallakora.com/Match-Center/?date={date}")
