4970a30f9fed94198dfd6f27bd30147b16e505eb
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-05-17 09:08:06 +0500

    ghgh

--- xgmodel.py
+++ xgmodel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler  # Import StandardScaler
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
 from category_encoders.target_encoder import TargetEncoder
@@ -10,18 +10,27 @@ from xgboost import XGBClassifier
 from xgboost import plot_importance
 
 df = pd.read_csv(r"C:\Users\HP\Documents\Master's thesis\collected data\collected data.csv")
-# print(df)
-# df['state'] = df['state'].replace({'successful': 1,'failed':0})
-# count = df['state'].value_counts()
+
+categorical_features = ["Project_name", "country"]
+le = LabelEncoder()
+for feature in categorical_features:
+    df[feature] = le.fit_transform(df[feature])
+
+numerical_features = ["backers_count", "goal", "pledged", "Average Contribution"]
+scaler = StandardScaler()
+df[numerical_features] = scaler.fit_transform(df[numerical_features])
+
 X = df.drop(columns='state')
 y = df['state']  
 le = LabelEncoder()
 y = le.fit_transform(y)
+y = y.replace({"failed": 0, "successful": 1})
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
 estimators = [
     ('encoder', TargetEncoder()),
+    ("scaler", StandardScaler()),
     ('clf', XGBClassifier(random_state=8))
 ]
 pipe = Pipeline(steps = estimators)
@@ -37,20 +46,25 @@ Search_space = {
     'clf__reg_lambda': Real(0.0, 10.0),
     'clf__gamma': Real(0.0, 10.0),
 }
+
 opt = BayesSearchCV(pipe, Search_space, cv=3, n_iter=10, scoring='roc_auc', random_state=8)
+
 opt.fit(X_train, y_train)
+
 opt.score(X_test, y_test)
-xgboost_step = opt.best_estimator_.steps[1]
+
+xgboost_step = opt.best_estimator_.steps[2]
+
 y_pred = opt.best_estimator_.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 auc = roc_auc_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
-tn = cm[0][0]  # True Negatives (correctly predicted failures)
-fp = cm[0][1]  # False Positives (failures predicted as success)
+tn = cm[0][0] 
+fp = cm[0][1]  
 specificity = tn / (tn + fp)
-
+print("Specificity:", specificity)
 print("Confusion Matrix:\n", cm)
 print("Accuracy:", accuracy)
 print("F1-Score:", f1)
