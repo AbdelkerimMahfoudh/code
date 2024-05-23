799028e6f918c3756f861cdcfb59842b2a47acad
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-05-23 11:52:54 +0500

    removing the pipeline

--- eval.py
+++ eval.py
@@ -31,26 +31,26 @@ y = le.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-estimators = [
-    ('encoder', TargetEncoder()),
-    ("scaler", StandardScaler()),
-    ('clf', XGBClassifier(random_state=8))
-]
-pipe = Pipeline(steps = estimators)
+# estimators = [
+#     ('encoder', TargetEncoder()),
+#     ("scaler", StandardScaler()),
+#     ('clf', XGBClassifier(random_state=8))
+# ]
+# pipe = Pipeline(steps = estimators)
 
-Search_space = {
-    'clf__max_depth' : Integer(2,8),
-    'clf__learning_rate':Real(0.001,1.0,prior='log-uniform'),
-    'clf__subsample': Real(0.5, 1.0),
-    'clf__colsample_bytree': Real(0.5, 1.0),
-    'clf__colsample_bylevel': Real(0.5, 1.0),
-    'clf__colsample_bynode': Real(0.5, 1.0),
-    'clf__reg_alpha': Real(0.0, 10.0),
-    'clf__reg_lambda': Real(0.0, 10.0),
-    'clf__gamma': Real(0.0, 10.0),
-}
+# Search_space = {
+#     'clf__max_depth' : Integer(2,8),
+#     'clf__learning_rate':Real(0.001,1.0,prior='log-uniform'),
+#     'clf__subsample': Real(0.5, 1.0),
+#     'clf__colsample_bytree': Real(0.5, 1.0),
+#     'clf__colsample_bylevel': Real(0.5, 1.0),
+#     'clf__colsample_bynode': Real(0.5, 1.0),
+#     'clf__reg_alpha': Real(0.0, 10.0),
+#     'clf__reg_lambda': Real(0.0, 10.0),
+#     'clf__gamma': Real(0.0, 10.0),
+# }
 
-opt = BayesSearchCV(pipe, Search_space, cv=3, n_iter=10, scoring='roc_auc', random_state=8)
+opt = XGBClassifier()
 evalset = [(X_train, y_train), (X_test,y_test)]
 opt.fit(X_train, y_train, eval_metric='logloss', eval_set=evalset)
 yhat = opt.predict(X_test)
