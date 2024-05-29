02b62222524f31f5bd018bfb0fc7596c70ea8383
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-05-30 03:41:51 +0500

    ds

--- xgmodel.py
+++ xgmodel.py
@@ -56,9 +56,9 @@ opt.score(X_test, y_test)
 
 xgboost_step = opt.best_estimator_.steps[2]
 
-y_pred = opt.best_estimator_.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+# y_pred = opt.best_estimator_.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# f1 = f1_score(y_test, y_pred)
 auc = roc_auc_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
