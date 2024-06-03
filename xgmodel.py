ac265a4fcc07592f0ff24556f7a5e5923760dff9
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-06-03 06:36:13 +0500

    set label

--- xgmodel.py
+++ xgmodel.py
@@ -74,8 +74,8 @@ print("Recall:", recall)
 # ax = sns.heatmap(cm, annot=True, cmap='Blues', fmt='d')
 # ax.set_title('XGBoost Confusion Matrix')
 # ax.set_xlabel('Predicted Label')
-# ax.set_ylabel('True Label')
-# plt.show()
+ax.set_ylabel('True Label')
+plt.show()
 model = opt.best_estimator_
 joblib.dump(model, "prediction.model")
 
